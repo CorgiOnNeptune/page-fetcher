c0257427e11587df8594c98074ff7a6ebb36ee68
Justin <justinjdbennett@gmail.com>
2022-11-24 02:18:14 +0000

    proper error messages and utilize cli args

--- fetcher.js
+++ fetcher.js
@@ -1,26 +1,35 @@
 // Should return the page downloaded after this command
 // node fetcher.js http://www.example.edu/ ./index.html
+
+const rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 const fs = require('fs');
 const request = require('request');
 const dir = process.cwd();
 
 const fetchHTML = (webpage, fileLocation) => request(webpage, (error, response, body) => {
-  console.log('requestBody:', body);
-  console.log('requestError:', error);
-  console.log('requestStatusCode:', response && response.statusCode);
-  writeToFile(body, fileLocation);
-  if (!error) {
-    console.log(`\nSuccess!\nFile written from ${webpage}\nTo ${dir}${fileLocation}\n`);
+  if (error) {
+    console.log('URL is invalid, unable to get response.');
+    process.exit();
   }
+
+  writeToFile(body, fileLocation, webpage);
 });
 
 
-const writeToFile = (content, fileLocation) => {
+const writeToFile = (content, fileLocation, webpage) => {
   fs.writeFile(fileLocation, content, err => {
     if (err) {
-      return console.error('Write Error: ', err);
+      console.error('Write Error: file path is invalid');
+      process.exit();
     }
+    console.log(`\nSuccess!\nFile written from ${webpage}\nDownloaded ${(content.length / 1000).toFixed(2)}MB to ${dir}${fileLocation}\n`);
+    process.exit();
   });
 };
 
-fetchHTML('http://www.example.edu', './fetchedFile.txt');
+const args = process.argv.slice(2);
+fetchHTML(args[0], args[1]);
